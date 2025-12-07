af29ef0e3ee6958b98318adc4b056bcb72af626c
royroyalle <21ke21i3387@21kschool.school>
2025-12-07 10:07:31 +0530

    .

---
+++ Lesson5/AfterClassActivity.py
@@ -0,0 +1,12 @@
+import math
+print("Welcome to Trignometry calculator")
+angle = int(input("Enter the angle of the triangle:"))
+angler = math.radians(angle)
+sin = math.sin(angler)
+cos = math.cos(angler)
+tan = math.tan(angler)
+print("After calculating the sin, cos and tan, these are the results:\n")
+print("Sin =", sin)
+print("Cos =", cos)
+print("Tan =", tan)
+
